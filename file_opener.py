3e4bcb637d184701ad3da5b9c61106be7c8368ba
bszeidl <szeidl.balazs@hotmail.com>
2023-06-14 21:15:18 +0200

    Lesson completed again.

--- file_opener.py
+++ file_opener.py
@@ -1,5 +1,6 @@
 def file_opener(file_name):
-    my_file = open(file_name)
-    my_file.close()
+    current_file = open(file_name)
+    print(current_file)
+    current_file.close()
 
 file_opener('text.txt')
